1a39386e71c32a27dbcf2bac55bda17255162add
calvin-r05 <158483246+calvin-r05@users.noreply.github.com>
2025-02-11 15:11:00 +0000

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MovieService } from './movie.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'movie-list';
+
+  movies:any[] = [];
+
+  constructor(private movieService:MovieService) {}
+
+  ngOnInit() {
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies)
+  }
+}
