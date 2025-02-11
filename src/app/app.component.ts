b0aaedad1b6c2839ab53118e86ffc6753a06a0c0
calvin-r05 <158483246+calvin-r05@users.noreply.github.com>
2025-02-11 16:45:22 +0000

    edede

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MovieService } from './movie.service';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, AddMovieComponent, ListMoviesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-list';
-
-  movies:any[] = [];
-
-  constructor(private movieService:MovieService) {}
-
-  ngOnInit() {
-    this.movies=this.movieService.getMovies();
-    console.log(this.movies)
-  }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -8,15 +8,17 @@ export class MovieService {
   constructor() { }
 
   moviesList = [
-    {id: 1, title: 'The Godfather', year: '1972', director: 'Francis Ford Coppola'}
+    {title: 'The Godfather', year: '1972', director: 'Francis Ford Coppola'}
    ,
-   {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'}
+   {title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'}
    ,
-   {id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'}
+   {title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'}
    ,
-   {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
+   {title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
   ];
-
+addMovie(movietitle:string, moviedirector:string, movieyear:string) {
+  this.moviesList.push({title:movietitle,director:moviedirector,year:movieyear})
+}
   getMovies() {
     return this.moviesList;
   }
